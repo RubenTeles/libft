a74cd132c68e4dae55e8d32901ca8e4bccbdd725
Ruben Teles <75394884+RubenTeles@users.noreply.github.com>
2021-12-23 18:06:29 +0000

    Add files via upload

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 19:48:49 by rteles            #+#    #+#             */
-/*   Updated: 2021/12/22 19:40:50 by rteles           ###   ########.fr       */
+/*   Updated: 2021/12/23 18:05:45 by rteles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	ft_memcmp(const void *str1, const void *str2, size_t n)
 	char	*s2;
 
 	i = 0;
+	a = 0;
 	s1 = (char *)str1;
 	s2 = (char *)str2;
 	while (i < n)
